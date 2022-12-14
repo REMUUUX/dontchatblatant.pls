b7e5c79c51f4cd4c707970eb6c07abe9e7c20498
REMUUUX <72861399+REMUUUX@users.noreply.github.com>
2022-12-14 11:17:54 +0100

    Add files via upload

---
+++ js/IPscript.js
@@ -0,0 +1,176 @@
+const textbox = document.getElementById("textbox");
+
+textbox.addEventListener("focus", () => {
+  textbox.style.backgroundColor = "rgb(33, 33, 33)";
+});
+
+textbox.addEventListener("blur", () => {
+  textbox.style.backgroundColor = "linear-gradient(to right, #f5f7fa, #c3cfe2)";
+});
+
+
+const textbox2 = document.getElementById("textbox2");
+
+// Get the button element from the DOM
+const button = document.getElementById('lookup');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+  // Initialize a new request to the API page
+  const request = new XMLHttpRequest();
+  request.open('GET', 'http://ip-api.com/line/'+textbox.value, true);
+
+  // When the request is finished, process the response
+  request.onload = function() {
+    // Check if the request was successful
+    if (request.status >= 200 && request.status < 400) {
+      // Get the API response as a string
+      const apiResponse = request.responseText;
+
+      // Separate the API response into an array of lines
+      const lines = apiResponse.split('\n');
+      // Set the textbox's value to the API response
+      textbox2.value = lines.join('\n');
+    } else {
+      // There was an error with the request, log it
+      console.error(request.statusText);
+    }
+  };
+
+  // Send the request
+  request.send();
+});
+
+  /* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+{
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#b61924",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+}
+
+);
