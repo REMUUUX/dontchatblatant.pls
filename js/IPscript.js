0a23657b6238bde95577f0e27e8d6fc3336c6dab
REMUUUX <tylkololjestnaj@gmail.com>
2022-12-14 20:26:02 +0100

    commit

---
+++ js/IPscript.js
@@ -0,0 +1,170 @@
+// get references to the input and output elements
+const textbox = document.getElementById('textbox');
+const outputbox = document.getElementById('output');
+
+// get reference to the lookup button
+const lookupButton = document.getElementById('lookup');
+
+// add a click event listener to the button
+lookupButton.addEventListener('click', function() {
+  // get the IP address from the input element
+  const ipAddress = textbox.value;
+
+  // make an HTTP GET request to the ipapi.co API
+  fetch('http://ip-api.com/line/'+ipAddress+'?fields=message,country,region,city,zip,lat,lon,isp,org,as,asname')
+    .then(response => response.text())  // parse the response as JSON
+    .then(text => {
+      // write the data to the output element
+      outputbox.innerHTML = text;
+
+    })
+    .catch(error => {
+      // if there was an error, write it to the output element
+      outputbox.innerHTML = error.message;
+    });
+});
+
+
+textbox.addEventListener("focus", () => {
+  textbox.style.backgroundColor = "rgb(33, 33, 33)";
+});
+
+textbox.addEventListener("blur", () => {
+  textbox.style.backgroundColor = "linear-gradient(to right, #f5f7fa, #c3cfe2)";
+});
+
+
+
+  /* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+{
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#b61924",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+}
+
+);
